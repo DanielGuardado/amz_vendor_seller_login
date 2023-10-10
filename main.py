1f20e864292e221ee70b9d6a41cec3a0b542e2ca
Daniel Guardado <danguardado217@gmail.com>
2023-10-10 14:27:54 -0400

    restructuring for newer chrome driver functionality

--- main.py
+++ main.py
@@ -3,22 +3,22 @@ from config.config import VENDOR_CENTRAL_CONFIG, SELLER_CENTRAL_CONFIG
 from selenium.webdriver.common.by import By
 from time import sleep
 
-vendor_central = AmazonManager(
-    VENDOR_CENTRAL_CONFIG["amazon_login"]["username"],
-    VENDOR_CENTRAL_CONFIG["amazon_login"]["password"],
-    VENDOR_CENTRAL_CONFIG["amazon_links"]["login_link"],
-    VENDOR_CENTRAL_CONFIG["amazon_xpaths"]["logged_in_xpath"],
-    VENDOR_CENTRAL_CONFIG["gmail_config"]["sender_email"],
-    VENDOR_CENTRAL_CONFIG["gmail_config"]["recipient_emails"],
-    VENDOR_CENTRAL_CONFIG["type"],
-)
+# vendor_central = AmazonManager(
+#     VENDOR_CENTRAL_CONFIG["amazon_login"]["username"],
+#     VENDOR_CENTRAL_CONFIG["amazon_login"]["password"],
+#     VENDOR_CENTRAL_CONFIG["amazon_links"]["login_link"],
+#     VENDOR_CENTRAL_CONFIG["amazon_xpaths"]["logged_in_xpath"],
+#     VENDOR_CENTRAL_CONFIG["gmail_config"]["sender_email"],
+#     VENDOR_CENTRAL_CONFIG["gmail_config"]["recipient_emails"],
+#     VENDOR_CENTRAL_CONFIG["type"],
+# )
 
-vendor_central.login()
-token = vendor_central.driver_actions.get_csrf_token(
-    VENDOR_CENTRAL_CONFIG["csrf_config"]["csrf_link"],
-    By.NAME,
-    VENDOR_CENTRAL_CONFIG["csrf_config"]["token_name"],
-)
+# vendor_central.login()
+# token = vendor_central.driver_actions.get_csrf_token(
+#     VENDOR_CENTRAL_CONFIG["csrf_config"]["csrf_link"],
+#     By.NAME,
+#     VENDOR_CENTRAL_CONFIG["csrf_config"]["token_name"],
+# )
 
 # print(token)
 
@@ -31,7 +31,7 @@ seller_central = AmazonManager(
     SELLER_CENTRAL_CONFIG["amazon_login"]["username"],
     SELLER_CENTRAL_CONFIG["amazon_login"]["password"],
     SELLER_CENTRAL_CONFIG["amazon_links"]["login_link"],
-    SELLER_CENTRAL_CONFIG["amazon_xpaths"]["logged_in_xpath"],
+    SELLER_CENTRAL_CONFIG["amazon_xpaths"],
     SELLER_CENTRAL_CONFIG["gmail_config"]["sender_email"],
     SELLER_CENTRAL_CONFIG["gmail_config"]["recipient_emails"],
     SELLER_CENTRAL_CONFIG["type"],
